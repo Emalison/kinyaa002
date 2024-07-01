5545db63129de71a9ae3c9f489c2996f5c7a1c47
Emalison <39349876+Emalison@users.noreply.github.com>
2024-07-01 05:33:15 +0300

    Update bet.py

--- bet.py
+++ bet.py
@@ -41,7 +41,7 @@ def job():
         options.add_argument("--no-sandbox")
         driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
         driver.set_window_size(1366, 768)
-        driver.get("https://splitthepot.games/betlion-ke/crashx/supersonic?token=982752c0-7c67-48ef-b924-bf7270a26821")
+        driver.get("https://splitthepot.games/betlion-ke/crashx/supersonic?token=8d69bfc6-fe56-4269-af27-107b8594b294")
         import time
         time.sleep(10)
         # Define the X and Y coordinates where you want to click
